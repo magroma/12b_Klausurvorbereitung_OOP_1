32e990d1ef129efcacf5e6f76b6a4c680a17d96d
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-06-10 10:26:45 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Test {
+
+	public void showAddress() {
+		System.out.println("Instanzvariable: " + this);
+		// this --> Instanzvariable
+		// verweist aus der Instanz auf sich selbst
+	}
+	
+}
